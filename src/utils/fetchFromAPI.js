1ed0d5df4a3aad2c8802b7c8cfb4cc17168c4cdb
Abel-Blue <abelmgetnet@gmail.com>
2023-01-07 22:51:20 +0300

    modified axios requests from the api

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,19 +1,22 @@
-import axios from 'axios';
-
-export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+const axios = require("axios");
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
-  params: {
-    maxResults: 50,
-  },
+  // url: BASE_URL,
+  params: {part: 'snippet', videoId: 'M7FIvfx5J10', maxResults: 50},
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-  },
+    'X-RapidAPI-Key': '7dd9d1469bmsh7dc0da7609fe390p1625c2jsn6e28f0a305bb',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
 };
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
 
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-
   return data;
-};
+}
